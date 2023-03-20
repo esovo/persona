856482d68234eb0b7c30f8a2e3f51ec7693d7fa8
김소진 <lunar7s@naver.com>
2023-03-20 13:44:26 +0900

    Feat: 헤더 작성 중

--- Frontend/persona/components/Header.tsx
+++ Frontend/persona/components/Header.tsx
@@ -1,13 +1,36 @@
 import Link from 'next/link';
+import style from './Header.module.scss';
+import { atom } from 'recoil';
+// import axios from 'axios';
 
 export default function Header() {
+  // const login = () => {
+  //   axios
+  // }
+
+  const isLogin = atom({
+    key: 'isLogin',
+    default: false,
+  });
+
   return (
-    <nav>
-      <Link href="/">PERSONA</Link>
-      <Link href="/practice">연기연습</Link>
-      <Link href="/community">커뮤니티</Link>
-      <Link href="/storage">보관함</Link>
-      <Link href="/bookmark">북마크</Link>
+    <nav className={style.nav}>
+      <div className={style.logo}>
+        <Link href="/">PERSONA</Link>
+      </div>
+      {isLogin ? (
+        <>
+          <div className={style.menu}>
+            <Link href="/practice">연기연습</Link>
+            <Link href="/community">커뮤니티</Link>
+            <Link href="/storage">보관함</Link>
+            <Link href="/bookmark">북마크</Link>
+          </div>
+          <button className={style.login}>시작하기</button>
+        </>
+      ) : (
+        <button>후하후하</button>
+      )}
     </nav>
   );
 }
