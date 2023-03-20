dbe22aaadacaac1168927a796bd6429b5d789258
김소진 <lunar7s@naver.com>
2023-03-20 15:27:28 +0900

    Feat: 헤더 구현 중

--- Frontend/persona/components/Header.tsx
+++ Frontend/persona/components/Header.tsx
@@ -1,35 +1,53 @@
 import Link from 'next/link';
 import style from './Header.module.scss';
-import { atom } from 'recoil';
-// import axios from 'axios';
+import { atom, useRecoilState } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
 
 export default function Header() {
-  // const login = () => {
-  //   axios
-  // }
+  const { persistAtom } = recoilPersist();
 
-  const isLogin = atom({
-    key: 'isLogin',
-    default: false,
+  const loginState = atom({
+    key: 'loginState',
+    default: true,
+    effects_UNSTABLE: [persistAtom],
   });
+
+  const userName = atom({
+    key: 'userName',
+    default: 'user',
+    effects_UNSTABLE: [persistAtom],
+  });
+
+  const [isLogin, setIsLogin] = useRecoilState(loginState);
+  const user = useRecoilState(userName);
+
+  const btnHandler = () => {
+    setIsLogin(!isLogin);
+  };
 
   return (
     <nav className={style.nav}>
       <div className={style.logo}>
         <Link href="/">PERSONA</Link>
       </div>
-      {isLogin ? (
-        <>
-          <div className={style.menu}>
-            <Link href="/practice">연기연습</Link>
-            <Link href="/community">커뮤니티</Link>
-            <Link href="/storage">보관함</Link>
-            <Link href="/bookmark">북마크</Link>
-          </div>
-          <button className={style.login}>시작하기</button>
-        </>
+      {!isLogin ? (
+        <div className={style.menu}>
+          <Link href="/practice">연기연습</Link>
+          <Link href="/community">커뮤니티</Link>
+          <Link href="/storage">보관함</Link>
+          <Link href="/bookmark">북마크</Link>
+        </div>
       ) : (
-        <button>후하후하</button>
+        <div className={style.menu} />
+      )}
+      {isLogin ? (
+        <button className={style.login} onClick={btnHandler}>
+          시작하기
+        </button>
+      ) : (
+        <button className={style.login} onClick={btnHandler}>
+          {user}
+        </button>
       )}
     </nav>
   );
