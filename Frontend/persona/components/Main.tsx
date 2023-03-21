80ad22c09265311acbd2b861a1a7411795bd1c54
cho50321 <cho50321@naver.com>
2023-03-22 01:26:46 +0900

    Feat: Main 컴포넌트 CSS 수정

--- Frontend/persona/components/Main.tsx
+++ Frontend/persona/components/Main.tsx
@@ -1,7 +1,7 @@
-/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/no-unescaped-entities */
 
 import style from '../styles/Main.module.scss';
+import Image from 'next/image';
 
 export default function Footer() {
   return (
@@ -14,16 +14,16 @@ export default function Footer() {
           지금 그 자체로도 매력적인 당신 <br />
           자연스러운 표정과 선명한 발음으로 한층 더 멋지게!
         </div>
-        <img src="Main_home.jpg" alt="home" width="1200" height="500" />
+        <Image src="/Main_home.jpg" alt="home" width="1200" height="500" />
       </div>
       <div className={style.introduce}>
         <h1>연기에 관심이 있는데 시작이 어려웠나요?</h1>
         <div>
-          퇴근 후 취미생활, 오디션 독학 준비 등으로 연기 연습을 해보고 싶다면 페르소나를 통해 지금 바로 연기를
-          시작해보세요!
+          퇴근 후 취미생활, 오디션 독학 준비 등으로 연기 연습을 해보고 싶다면 <br />
+          페르소나를 통해 지금 바로 연기를 시작해보세요! <br />
+          온라인으로 시간과 공간에 제약을 받지 않아 자유롭게 연습할 수 있고,
           <br />
-          온라인으로 시간과 공간에 제약을 받지 않아 자유롭게 연습할 수 있고, AI 감정 분석 등 다양한 기능을 제공받을 수
-          있습니다.
+          AI 감정 분석 등 다양한 기능을 제공받을 수 있습니다.
         </div>
       </div>
       <div className={style.smart}>
@@ -37,19 +37,19 @@ export default function Footer() {
           </div>
         </div>
         <div className={style.right}>
-          <img src="Main_smart.jpg" alt="smart" width="500" height="350" />
+          <Image src="/Main_smart.jpg" alt="smart" width="500" height="350" />
         </div>
       </div>
       <div className={style.easy}>
         <div className={style.left}>
-          <img src="Main_easy.jpg" alt="easy" width="500" height="350" />
+          <Image src="/Main_easy.jpg" alt="easy" width="500" height="350" />
         </div>
         <div className={style.right}>
           <div className={style.title}>#SO EASY</div>
           <div className={style.subtitle}>대본부터 연기 연습까지 하나로!</div>
           <div className={style.content}>
             대본 준비부터 대본 분석, 연습까지 따로 하셨나요? <br />
-            페르소나는 대 본부터 분석, 연습 프로그램까지 제공합니다. <br />
+            페르소나는 대본부터 분석, 연습 프로그램까지 제공합니다. <br />
             이제 여러가지 준비할 것 없이 쉽게 연기를 연습해보세요.
           </div>
         </div>
@@ -60,17 +60,19 @@ export default function Footer() {
           <div className={style.subtitle}>모니터링은 선택이 아닌 필수</div>
           <div className={style.content}>
             실제 연기 학원에서도 진행하는 모니터링. <br />
-            연기는 연습 뿐만 아니라 자신의 연기를 직접 모니터링 하며 피드백하는 과정도 필요합니다! <br />
-            연기 연습 후 영상과 대본 분석을 보관할 수 있고, 이를 통해 셀프 피드백이 가능합니다.
+            연기는 연습 뿐만 아니라 자신의 연기를 직접 모니터링 하며 <br />
+            피드백하는 과정도 중요합니다! <br />
+            연기 연습 후 영상과 대본 분석을 보관할 수 있고, <br />
+            이를 통해 셀프 피드백이 가능합니다.
           </div>
         </div>
         <div className={style.right}>
-          <img src="Main_simple.jpg" alt="simple" width="500" height="400" />
+          <Image src="/Main_simple.jpg" alt="simple" width="500" height="400" />
         </div>
       </div>
       <div className={style.exciting}>
         <div className={style.left}>
-          <img src="Main_exciting.jpg" alt="exciting" width="500" height="350" />
+          <Image src="/Main_exciting.jpg" alt="exciting" width="500" height="350" />
         </div>
         <div className={style.right}>
           <div className={style.title}>#EXCITING</div>
@@ -78,7 +80,8 @@ export default function Footer() {
           <div className={style.content}>
             연기라는 같은 목적을 가진 사람들과 소통할 수 있는 공간. <br />
             궁금한 점부터 피드백까지 다양한 이야기를 나눌 수 있습니다! <br />
-            보관함에 있는 나의 연기를 다른 사람들과 공유할 수 있어 쉽고 빠르게 피드백을 받을 수 있습니다. <br />
+            보관함에 있는 나의 연기를 다른 사람들과 공유할 수 있어 <br />
+            쉽고 빠르게 피드백을 받을 수 있습니다. <br />
           </div>
         </div>
       </div>
