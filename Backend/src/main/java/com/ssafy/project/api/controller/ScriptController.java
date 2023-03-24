6757d3cd38931fc7b2addfec5ffe018e52e5e6c5
NAM <nkj0901@naver.com>
2023-03-24 12:56:05 +0900

    fix: db 설정 수정

--- Backend/src/main/java/com/ssafy/project/api/controller/ScriptController.java
+++ Backend/src/main/java/com/ssafy/project/api/controller/ScriptController.java
@@ -1,8 +1,12 @@
 package com.ssafy.project.api.controller;
 
+import com.ssafy.project.common.util.Msg;
+import com.ssafy.project.common.util.ResponseDTO;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class ScriptController {
 
     //대본 조회
+//    public ResponseEntity<ResponseDTO> scriptList(){
+//        Page<ScriptDTO> scripts = scriptService.findAllScript(page, sort);
+//        return ResponseEntity.ok().body(ResponseDTO.of(HttpStatus.OK, Msg.SUCCESS_SCRIPT_READ, scripts));
+//    }
 
-    //대본 등록
-    //대본 수정
-    //대본 삭제
-    //내 대본 보기
+    //내가 참여한 대본
 }
