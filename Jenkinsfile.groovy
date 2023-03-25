8d91d81d0971e5ac48ae5784673c13a689975617
김소진 <lunar7s@naver.com>
2023-03-25 17:14:12 +0900

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,13 +16,13 @@ pipeline {
 
     stage('Npm Build') {
       steps {
-        sh """
-          cd \"${env.WORKSPACE}/Frontend/persona\"
-          mkdir -p .npm
-          chown -R 113:119 .npm
+        sh '''
+          cd "${env.WORKSPACE}/Frontend/persona"
+          sudo mkdir -p .npm
+          sudo chown -R 113:119 .npm
           npm install
           npm run build
-        """
+        '''
       }
     }
 
