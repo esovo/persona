9858fc5ca4423e7d42070d1a5d6fcb9e36c63319
조은서 <cho50321@naver.com>
2023-03-28 15:18:13 +0900

    Feat: 커뮤니티 게시글 및 CSS 구현

--- Frontend/persona/pages/community/index.tsx
+++ Frontend/persona/pages/community/index.tsx
@@ -2,6 +2,9 @@
 import { useState, FormEvent } from 'react';
 import { useRecoilState } from 'recoil';
 import { postmodal } from '../../states/communityState';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import Header from '@/components/Header';
 import Modal from '@/components/PostModal';
 import Footer from '@/components/Footer';
@@ -33,10 +36,14 @@ export default function List() {
 
   const posts: Post[] = [
     { id: 1, nickname: 'ovo', time: '2023-03-24', title: '테스트', body: '테스트 글입니다.', like: 1, comment: 0 },
+    { id: 2, nickname: 'ovo6', time: '2023-03-28', title: '테스트2', body: '테스트2 글입니다.', like: 2, comment: 0 },
+    { id: 3, nickname: 'ovovo', time: '2023-03-28', title: '테스트3', body: '테스트3 글입니다.', like: 3, comment: 0 },
+    { id: 4, nickname: 'ovovo', time: '2023-03-28', title: '테스트4', body: '테스트4 글입니다.', like: 4, comment: 0 },
+    { id: 5, nickname: 'ovovo', time: '2023-03-28', title: '테스트5', body: '테스트5 글입니다.', like: 5, comment: 0 },
   ];
 
   return (
-    <>
+    <div className={style.wrapper}>
       <Header />
       {showModal && <Modal />}
       <div className={style.intro}>
@@ -48,28 +55,30 @@ export default function List() {
           <form className={style.searchForm} onSubmit={handleSubmit} autoComplete="off">
             <div className={style.searchInput}>
               <input
+                className={style.searchText}
                 type="text"
                 id="input_search"
                 placeholder="글 제목, 글 내용, 작성자 검색"
                 maxLength={200}
                 autoComplete="off"
-                className={style.searchText}
                 value={searchQuery}
                 onChange={(event) => setSearchQuery(event.target.value)}
               />
-              <button type="submit" className="search">
-                검색
+              <button type="submit" className={style.search}>
+                <FontAwesomeIcon icon={faMagnifyingGlass as IconProp} style={{ color: '#5e5e5e' }} />
               </button>
             </div>
           </form>
           <div className={style.writePost}>
-            <button onClick={startHandler}>새로운 글을 남겨보세요.</button>
+            <button className={style.writeBtn} onClick={startHandler}>
+              새로운 글을 남겨보세요.
+            </button>
           </div>
-          <div className={style.post}>
+          <div className={style.posts}>
             {posts.map((post) => (
-              <div key={post.id}>
-                <div>{post.nickname}</div>
+              <div key={post.id} className={style.post}>
                 <h3>{post.title}</h3>
+                <div>{post.nickname}</div>
                 <div>{post.body}</div>
                 <div className={style.itmes}>
                   <div>{post.like}</div>
@@ -79,9 +88,29 @@ export default function List() {
             ))}
           </div>
         </div>
-        <div className="right"></div>
+        <div className={style.right}>
+          <div className={style.popular}>
+            <div className={style.more}>
+              <div>인기글</div>
+              <div>더보기</div>
+            </div>
+            <div className={style.posts}>
+              {posts.map((post) => (
+                <div key={post.id} className={style.post}>
+                  <div>{post.nickname}</div>
+                  <h3>{post.title}</h3>
+                  <div>{post.body}</div>
+                  <div className={style.itmes}>
+                    <div>{post.like}</div>
+                    <div>{post.comment}</div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
