54fe82ef1e2b92654fb5f13362e4f5501820cffe
Oliverslife <voise.ksing@gmail.com>
2023-04-01 16:37:29 +0900

    Feat: 대본정보 구현 중

--- AI/face-emotion-recognition/src/components/PracticePage/Script.jsx
+++ AI/face-emotion-recognition/src/components/PracticePage/Script.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import style from './Script.module.scss';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+
 export default function Script(props) {
     return (
         <div className={style.container}>
             <div className={style.newandbookmark}>
                 <div className={style.new}>NEW</div>
-                <div className={style.bookmark}></div>
-
+                <div className={style.bookmark}><FontAwesomeIcon icon={faHeart} /></div>
             </div>
             <div className={style.date}>
 
