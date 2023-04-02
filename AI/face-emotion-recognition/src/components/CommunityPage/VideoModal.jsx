cd3fe56a1032fc78bffdfedea323e00b88f2ea1b
cho50321 <cho50321@naver.com>
2023-04-02 23:49:06 +0900

    Feat: 커뮤니티 CSS 수정 및 내 영상 가져오기(Video) 컴포넌트 구현

--- AI/face-emotion-recognition/src/components/CommunityPage/VideoModal.jsx
+++ AI/face-emotion-recognition/src/components/CommunityPage/VideoModal.jsx
@@ -1,1 +1,48 @@
-showVideoModal
+// import axios from 'axios';
+import React, { useState } from 'react';
+import style from './VideoModal.module.scss';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { videoModal, videosState } from '../../states/communityState';
+import Video from './Video';
+
+export default function Modal() {
+  const [showModal, setShowModal] = useRecoilState(videoModal);
+  const videos = useRecoilValue(videosState);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const shutModal = () => {
+    setShowModal(false);
+  };
+
+  const handleWrite = () => {
+    // 선택된 비디오 데이터를 사용하여 다른 컴포넌트에서 처리할 수 있습니다.
+    console.log(selectedVideo);
+  };
+
+  return (
+    <>
+      {showModal && (
+        <div className={style.back}>
+          <div className={style.container}>
+            <div className={style.header}>
+              <div className={style.title}>내 영상 가져오기</div>
+            </div>
+            <div className={style.videos}>
+              {videos.map((video) => (
+                <Video key={video.id} {...video} onSelect={setSelectedVideo} />
+              ))}
+            </div>
+            <div className={style.bottom}>
+              <button className={style.close} onClick={shutModal}>
+                취소
+              </button>
+              <button className={style.write} onClick={handleWrite}>
+                게시
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
--- AI/face-emotion-recognition/src/states/communityState.js
+++ AI/face-emotion-recognition/src/states/communityState.js
@@ -141,3 +141,58 @@ export const commentsState = atom({
     },
   ],
 });
+
+export const videosState = atom({
+  key: 'videosState',
+  default: [
+    {
+      id: 1,
+      createDate: '2023-04-02 16:28:28',
+      title: '영상 1',
+      imgUrl: 'kurome.jpg',
+      url: 'server Url',
+    },
+    {
+      id: 2,
+      createDate: '2023-04-02 16:30:28',
+      title: '영상 2',
+      imgUrl: 'mymelody.jpg',
+      url: 'server Url',
+    },
+    {
+      id: 3,
+      createDate: '2023-04-02 16:32:28',
+      title: '영상 3',
+      imgUrl: 'cinnamoroll.jpg',
+      url: 'server Url',
+    },
+    {
+      id: 4,
+      createDate: '2023-04-02 16:38:28',
+      title: '영상 4',
+      imgUrl: 'hellokitty.jpg',
+      url: 'server Url',
+    },
+    {
+      id: 3,
+      createDate: '2023-04-02 16:32:28',
+      title: '영상 5',
+      imgUrl: 'cinnamoroll.jpg',
+      url: 'server Url',
+    },
+    {
+      id: 4,
+      createDate: '2023-04-02 16:38:28',
+      title: '영상 6',
+      imgUrl: 'hellokitty.jpg',
+      url: 'server Url',
+    },
+    {
+      id: 3,
+      createDate: '2023-04-02 16:32:28',
+      title: '영상 7',
+      imgUrl: 'cinnamoroll.jpg',
+      url: 'server Url',
+    },
+  ],
+});
