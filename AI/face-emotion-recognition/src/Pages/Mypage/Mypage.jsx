31dbe440923d2c2a0c5907b66fdfe9ed71f47946
mayril <inho806901@gmail.com>
2023-04-03 15:44:54 +0900

    commit

--- AI/face-emotion-recognition/src/Pages/Mypage/Mypage.jsx
+++ AI/face-emotion-recognition/src/Pages/Mypage/Mypage.jsx
@@ -1,7 +1,16 @@
 import { useRecoilState } from 'recoil';
 
 import style from './Mypage.module.scss';
+import BasicList from '../../components/List/BasicList';
 
 export default function Mypage() {
+  return(
+    <div>    
+    <BasicList>
 
+    </BasicList>
+    </div>
+  );
 };
+
+
