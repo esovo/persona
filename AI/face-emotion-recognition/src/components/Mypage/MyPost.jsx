92c1fb024983f5425d8cb5c25996bc076d7d3b0f
lunar7s@naver.com <lunar8s@naver.com>
2023-04-04 13:58:36 +0900

    Feat: 로컬테스트

---
+++ AI/face-emotion-recognition/src/components/Mypage/MyPost.jsx
@@ -0,0 +1,17 @@
+
+
+import style from './MyPost.module.scss';
+
+export default function MyPost() {
+
+    return (
+        <div className={style.outline}>
+            <div className={style.container}>
+
+
+            </div>
+
+        </div>
+
+    );
+}
