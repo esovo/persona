291fef080d1300569f3496192dd6831df9fe7b54
이상학 <dltkdgkr_@naver.com>
2023-04-05 17:12:23 +0900

    fix: rm testapi/securityconfig

--- Backend/src/main/java/com/ssafy/project/common/config/SecurityConfig.java
+++ Backend/src/main/java/com/ssafy/project/common/config/SecurityConfig.java
@@ -98,7 +98,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**",
-                        "/board/all", "/board/top", "oauth2/**")
+                        "/oauth2/**", "/board/all", "/board/top")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
