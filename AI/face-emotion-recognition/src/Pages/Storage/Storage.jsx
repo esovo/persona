dcef2f7350c69996d97cdc1733a14664109f4888
김소진 <lunar7s@naver.com>
2023-04-06 00:14:54 +0900

    Update Storage.jsx

--- AI/face-emotion-recognition/src/Pages/Storage/Storage.jsx
+++ AI/face-emotion-recognition/src/Pages/Storage/Storage.jsx
@@ -8,10 +8,8 @@ import Video from '../../components/Storage/Video';
 // import Footer from "../../components/Common/Footer";
 import style from './Storage.module.scss';
 import Pagination from "@mui/material/Pagination";
-import { useEffect } from 'react';
 import { videoApis } from '../../apis/videoApis';
 import axios from 'axios';
-import { tokenState } from '../../states/loginState';
 import { useNavigate } from 'react-router-dom';
 
 export default function List() {
