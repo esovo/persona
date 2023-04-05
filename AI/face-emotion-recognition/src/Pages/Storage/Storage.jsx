646492eeb3a94a69a995614de4db852a4b000dbe
NAM <nkj0901@naver.com>
2023-04-06 00:28:45 +0900

    fix: import 중복 제거

--- AI/face-emotion-recognition/src/Pages/Storage/Storage.jsx
+++ AI/face-emotion-recognition/src/Pages/Storage/Storage.jsx
@@ -8,7 +8,6 @@ import Video from '../../components/Storage/Video';
 // import Footer from "../../components/Common/Footer";
 import style from './Storage.module.scss';
 import Pagination from "@mui/material/Pagination";
-import { useEffect } from 'react';
 import { videoApis } from '../../apis/videoApis';
 import axios from 'axios';
 import { tokenState } from '../../states/loginState';
