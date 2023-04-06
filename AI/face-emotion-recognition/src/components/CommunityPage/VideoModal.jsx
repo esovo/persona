737880e88272c74b83e041b74785dbbe91c74b55
조은서 <cho50321@naver.com>
2023-04-06 14:44:46 +0900

    Feat: 게시글 쓰기 내 영상 가져오기 API 통신

--- AI/face-emotion-recognition/src/components/CommunityPage/VideoModal.jsx
+++ AI/face-emotion-recognition/src/components/CommunityPage/VideoModal.jsx
@@ -1,19 +1,33 @@
-// import axios from 'axios';
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import style from './VideoModal.module.scss';
 import { useRecoilState, useRecoilValue } from 'recoil';
-import { videoModal, videosState } from '../../states/communityState';
+import { videoModal } from '../../states/communityState';
+import { videoApis } from '../../apis/videoApis';
+import { tokenState } from '../../states/loginState';
 import Video from './Video';
 
 export default function Modal() {
   const [showModal, setShowModal] = useRecoilState(videoModal);
-  const videos = useRecoilValue(videosState);
+  const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
+  const token = useRecoilValue(tokenState);
 
   const shutModal = () => {
     setShowModal(false);
   };
 
+  useEffect(() => {
+    const page = 0;
+    axios
+      .get(videoApis.VIDEO_GET_API(page), {
+        headers: { Authorization: token },
+      })
+      .then((res) => {
+        console.log(res.data.value.content);
+        setVideos(res.data.value.content);
+      });
+  }, []);
   const handleWrite = () => {
     // 선택된 비디오 데이터를 사용하여 다른 컴포넌트에서 처리할 수 있습니다.
     console.log(selectedVideo);
